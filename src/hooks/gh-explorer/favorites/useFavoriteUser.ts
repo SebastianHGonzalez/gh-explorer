d7f62d4ab5c5f3d9b9ac61c5004093c978b1d9f4
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2025-06-03 14:08:05 +0100

    Fix: refetch favorites list on favorites change

--- src/hooks/gh-explorer/favorites/useFavoriteUser.ts
+++ src/hooks/gh-explorer/favorites/useFavoriteUser.ts
@@ -1,12 +1,14 @@
+import { listFavoritesQuery } from "@/apis/gh-explorer/favorites";
 import { addFavoriteMutation } from "@/apis/gh-explorer/favorites/add";
 import { isFavoritesQuery } from "@/apis/gh-explorer/favorites/is";
 import { removeFavoriteMutation } from "@/apis/gh-explorer/favorites/remove";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useMutation, useQuery } from "@tanstack/react-query";
 import { useSQLiteContext } from "expo-sqlite";
 
 export function useFavoriteUser(login: string) {
   const db = useSQLiteContext();
   const user = { login };
+  const listFavorites = useInfiniteQuery({ ...listFavoritesQuery(db), enabled: false });
   const isFavorite = useQuery(isFavoritesQuery(db, user));
   const addFavorite = useMutation(addFavoriteMutation(db, user));
   const removeFavorite = useMutation(removeFavoriteMutation(db, user));
@@ -18,10 +20,12 @@ export function useFavoriteUser(login: string) {
     addFavorite: () =>
       addFavorite.mutateAsync().then(() => {
         isFavorite.refetch();
+        listFavorites.refetch();
       }),
     removeFavorite: () =>
       removeFavorite.mutateAsync().then(() => {
         isFavorite.refetch();
+        listFavorites.refetch();
       }),
   };
 }
