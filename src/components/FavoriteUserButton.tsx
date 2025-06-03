bc7da5ec76bc10dc63a62576f7ccaa7665410c54
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2025-06-03 19:18:42 +0100

    Added favorite button animation

--- src/components/FavoriteUserButton.tsx
+++ src/components/FavoriteUserButton.tsx
@@ -3,6 +3,11 @@ import { t } from "@/i18n/t";
 import { useCallback } from "react";
 import { StyleProp, TouchableOpacity, ViewStyle } from "react-native";
 import { useTheme } from "react-native-paper";
+import Animated, {
+    useAnimatedStyle,
+    useSharedValue,
+    withSpring,
+} from "react-native-reanimated";
 import { AppIcon } from "./common/AppIcon";
 
 interface FavoriteUserButtonProps {
@@ -15,11 +20,15 @@ export function FavoriteUserButton({ login, style }: FavoriteUserButtonProps) {
   const { isFavorite, isLoading, isUpdating, addFavorite, removeFavorite } =
     useFavoriteUser(login);
 
+  const animation = useFavoriteUserAnimation();
+
   const handleRemoveFavorite = useCallback(() => {
     removeFavorite();
+    animation.onUnfavorite();
   }, [removeFavorite]);
   const handleAddFavorite = useCallback(() => {
     addFavorite();
+    animation.onFavorite();
   }, [addFavorite]);
 
   return (
@@ -27,13 +36,37 @@ export function FavoriteUserButton({ login, style }: FavoriteUserButtonProps) {
       onPress={isFavorite ? handleRemoveFavorite : handleAddFavorite}
       accessibilityLabel={t("FavoriteUserButton.accessibilityLabel")}
       disabled={isLoading || isUpdating}
-      style={[{ opacity: isLoading || isUpdating ? 0.5 : 1 }, style]}
+      style={style}
     >
-      <AppIcon
-        name={isFavorite ? "favorited" : "unfavorited"}
-        size="xl"
-        color={isFavorite ? "primary" : "onSurface"}
-      />
+      <Animated.View style={animation.style}>
+        <AppIcon
+          name={isFavorite ? "favorited" : "unfavorited"}
+          size="xl"
+          color={isFavorite ? "primary" : "onSurface"}
+        />
+      </Animated.View>
     </TouchableOpacity>
   );
+}
+
+function useFavoriteUserAnimation() {
+  const scale = useSharedValue(1);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
+  return {
+    style: animatedStyle,
+    onFavorite: () => {
+      scale.value = withSpring(1.2, { duration: 50 }, () => {
+        scale.value = withSpring(1);
+      });
+    },
+    onUnfavorite: () => {
+      scale.value = withSpring(0.8, { duration: 50 }, () => {
+        scale.value = withSpring(1);
+      });
+    },
+  } as const;
 }
